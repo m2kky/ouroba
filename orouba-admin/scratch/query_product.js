78ad122baa3ba3c4f2e95484408fb7c826bd6555
m2kky <Muhammedmekky4@gmail.com>
2026-05-17 18:06:35 +0300

    fix: remove scratch dir from repo to fix Nixpacks UTF-8 error

--- orouba-admin/scratch/query_product.js
+++
@@ -1,5 +0,0 @@
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
-prisma.product.findFirst({ include: { recommendedRecipes: true, images: true } })
-  .then(p => console.log(JSON.stringify(p, null, 2)))
-  .finally(() => prisma.$disconnect());
