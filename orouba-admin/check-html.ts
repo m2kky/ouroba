4809e91f87fc3ba476fe0af82a99f2daee5253b3
m2kky <Muhammedmekky4@gmail.com>
2026-05-24 18:15:12 +0300

    chore: remove scratch files and scripts to fix Nixpacks deployment

--- orouba-admin/check-html.ts
+++
@@ -1,9 +0,0 @@
-import { PrismaClient } from '@prisma/client';
-
-const prisma = new PrismaClient();
-
-async function main() {
-  console.log(await prisma.recipe.findUnique({where:{id:'60'}}).then(r=>r?.descriptionAr));
-}
-
-main().catch(console.error).finally(() => prisma.$disconnect());
--- orouba-admin/check_recipe_img.js
+++
@@ -1,9 +0,0 @@
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
-
-async function main() {
-  const all = await prisma.recipe.findMany({ select: { id: true, nameAr: true, internalImage: true }});
-  console.log(all.slice(0, 5));
-  await prisma.$disconnect();
-}
-main();
